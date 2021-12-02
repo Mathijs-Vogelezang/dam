9bf5892d85bd8f21bad514ad7c002c7533996600
Mathijs-Vogelezang <70201164+Mathijs-Vogelezang@users.noreply.github.com>
2021-12-02 16:39:08 +0100

    started on game structure

--- src/Board.java
+++ src/Board.java
@@ -1,2 +1,15 @@
 public class Board {
+    public static int SIZE = 10;
+    private Piece[][] board = new Piece[SIZE][SIZE];
+
+    public void generateBoard() {
+        int height = SIZE / 2 - 1; // The starting height for the rows
+
+        for (int i = 0; i < height; i++) {
+            for (int j = i % 2; j < SIZE; j += 2) {
+                board[i][j] = new Piece(Colors.WHITE);
+                board[height + 2 + i][j] = new Piece(Colors.BLACK);
+            }
+        }
+    }
 }
---
+++ src/Piece.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+
+public class Piece {
+    private Colors color;
+    private boolean isKing = false;
+
+    public Piece(Colors color) {
+        this.color = color;
+    }
+
+}
